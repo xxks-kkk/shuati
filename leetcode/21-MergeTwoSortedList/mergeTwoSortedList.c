beadd9447010ebd84afa0a3b867d039acce5a6c6
Zeyuan On Hadoop <ferrishu3886@gmail.com>
2017-06-14 22:58:24 +0800

    Add 21

---
+++ leetcode/21-MergeTwoSortedList/mergeTwoSortedList.c
@@ -0,0 +1,35 @@
+/*
+ * [Source]
+ * 
+ * https://leetcode.com/problems/merge-two-sorted-lists
+ *
+ * [Problem Description]
+ *
+ * Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+ * 
+ * [Companies]
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2)
+{
+  if (l1 == NULL) return l2;
+  if (l2 == NULL) return l1;
+
+  if (l1->val < l2->val)
+  {
+    l1->next = mergeTwoLists(l1->next, l2);
+    return l1;
+  }
+  else
+  {
+    l2->next = mergeTwoLists(l1, l2->next);
+    return l2;
+  }
+}
