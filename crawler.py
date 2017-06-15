e0bd5b3003ac2f4d7289ccb858938f07e30735db
Zeyuan On Hadoop <ferrishu3886@gmail.com>
2017-06-15 15:36:41 +0800

    finish crawler.py

--- crawler.py
+++ crawler.py
@@ -1,40 +1,87 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+
+# Quick leetcode crawler
+#
+# Generate the source file of the question and pre-fixed head comment
+# 
+# Usage: crawler.py [leetcode question url]
+
 from bs4 import BeautifulSoup
 import requests
-#import re
+import sys
+#from sys import argv
+import re
+#from urllib.parse import urlparse
+import validators
+import operator
+import os
 
-#url = "https://leetcode.com/problems/reverse-integer/"
-url="https://leetcode.com/problems/reverse-linked-list-ii/#/description"
-r = requests.get(url)
-data = r.text
-soup = BeautifulSoup(data, "lxml")
+if __name__ == "__main__":
 
-letters = soup.find_all("div", class_="question-content")
-print(letters[0])
+    script, url = sys.argv
+    #print('url is {:s}'.format(url))
 
-for element in letters:
-    rawText = element.get_text()
+    if not validators.url(url):
+        print("Please enter a valid url!")
+        sys.exit()
+        
+    #url = "https://leetcode.com/problems/reverse-integer/"
+    #url = "https://leetcode.com/problems/reverse-linked-list-ii/#/description"
+    r = requests.get(url)
+    data = r.text
+    soup = BeautifulSoup(data, "lxml")
 
-lines = rawText.split('\n') # Split into lines: https://stackoverflow.com/questions/172439/how-do-i-split-a-multi-line-string-into-multiple-lines
-lines = filter(None, lines) # Removes empty strings: https://stackoverflow.com/questions/3845423/remove-empty-strings-from-a-list-of-strings
+    title_corp = soup.find_all("div", class_="question-title")
+    title_raw = title_corp[0].h3.get_text()
+    title_lines = title_raw.split('\n')
+    title_lines = list(filter(operator.methodcaller('strip'), title_lines)) # https://stackoverflow.com/questions/8449454/remove-strings-containing-only-white-spaces-from-list
+    title_rdy = title_lines[0].lstrip(' ').replace(".", "-").split(' ')
+    #print(title_rdy)
+    title = "".join(title_rdy)
+    #print(title)
+    
+    path = "./leetcode/" + title
+    os.mkdir(path)
+    
+    extension = ".c"
+    pat = re.compile(r"^(\d+)-")
+    m = re.search(pat, title)
+    filename=title[:m.start()] + title[m.end():]
+    filename=filename[0].lower() + filename[1:]
+    #print(filename)
+    target = open(path+"/"+filename+extension, "w")
+    
+    letters = soup.find_all("div", class_="question-content")
+    #print(letters[0])
 
-target = open("leetcode.test", "w")
-target.write("/*          \n")
-target.write(" * [Source] \n")
-target.write(" *          \n")
-target.write(" * " + url + "\n")
-target.write(" *          \n")
-target.write(" * [Problem Description]\n");
-target.write(" *          \n")
+    rawText = letters[0].get_text()
 
-for line in lines:
-    if "Subscribe" not in line:
-        if "click" in line:
-            pass
+    lines = rawText.split('\n') # Split into lines: https://stackoverflow.com/questions/172439/how-do-i-split-a-multi-line-string-into-multiple-lines
+    lines = list(filter(None, lines)) # Removes empty strings: https://stackoverflow.com/questions/3845423/remove-empty-strings-from-a-list-of-strings
+
+    target.write("/*          \n")
+    target.write(" * [Source] \n")
+    target.write(" *          \n")
+    target.write(" * " + url + "\n")
+    target.write(" *          \n")
+    target.write(" * [Problem Description]\n");
+    target.write(" *          \n")
+
+    for line in lines:
+        if "Subscribe" not in line:
+            if "click" in line:
+                pass
+            else:
+                # Remove CLR, "\n" if appears
+                line = line.replace("\r", "").replace("\n", "")
+                #print(line)
+                target.write(" * " + line + "\n") # https://stackoverflow.com/questions/31137552/unicodeencodeerror-ascii-codec-cant-encode-character-at-special-name
         else:
-            target.write(" * " + line.encode('utf-8') + "\n") # https://stackoverflow.com/questions/31137552/unicodeencodeerror-ascii-codec-cant-encode-character-at-special-name
-    else:
-        break
-target.write(" *          \n")
-target.write(" * [Companies]\n");
-target.write(" */          \n")
-target.close()
+            break
+    target.write(" *          \n")
+    target.write(" * [Companies]\n");
+    target.write(" */          \n")
+    target.write("\n\n")
+    
+    target.close()
