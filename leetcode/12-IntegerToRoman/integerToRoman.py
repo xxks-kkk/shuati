054037b5a6afdc0a9440aca0ba602f8196636124
YangHu-yh <34641580+YangHu-yh@users.noreply.github.com>
2017-12-24 19:48:11 -0600

    Add 12 (#7)
    * Add 11
    
    * add pic
    
    * Update .gitignore
    
    * cleanup
    
    * Add pic in README
    
    * Add 15
    
    * Add 16
    
    * add 12
    
    * Update 3Sum.py

---
+++ leetcode/12-IntegerToRoman/integerToRoman.py
@@ -0,0 +1,20 @@
+# Given an integer, convert it to a roman numeral.
+#
+# Input is guaranteed to be within the range from 1 to 3999.
+
+
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        I = ["","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        X = ["","X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        C = ["","C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        M = ["", "M", "MM", "MMM"]
+        return M[num//1000] + C[num % 1000//100] + X[num % 100//10] + I[num % 10]
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.intToRoman(1990))
