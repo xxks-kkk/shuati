2e6548d525a63a71b26092cd45b9f7b8a7bea121
Zeyuan Hu <ferrishu3886@gmail.com>
2018-01-14 21:46:08 +0800

    Add 89

---
+++ leetcode/89-GrayCode/README.md
@@ -0,0 +1,12 @@
+## Solution explanation
+
+1st link in Reference section has a detail summarization of the solutions. Here, I implemented
+the [binary to gray code algorithm](https://en.wikipedia.org/wiki/Gray_code#Converting_to_and_from_Gray_code) 
+on Wikipedia.
+
+
+## Reference
+
+- http://www.cnblogs.com/grandyang/p/4315649.html
+- https://en.wikipedia.org/wiki/Gray_code
+- https://leetcode.com/problems/gray-code/discuss/29893
---
+++ leetcode/89-GrayCode/grayCode.py
@@ -0,0 +1,27 @@
+# The gray code is a binary numeral system where two successive values differ in only one bit.
+#
+# Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. A gray code sequence must begin with 0.
+#
+# For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+#
+# 00 - 0
+# 01 - 1
+# 11 - 3
+# 10 - 2
+# Note:
+# For a given n, a gray code sequence is not uniquely defined.
+#
+# For example, [0,2,3,1] is also a valid gray code sequence according to the above definition.
+#
+# For now, the judge is able to judge based on one instance of gray code sequence. Sorry about that.
+
+class Solution:
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        res = []
+        for i in range(2**n):
+            res.append(i^(i>>1))
+        return res
