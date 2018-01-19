248428c4185d49d2d65e63c5e8adad3956592d81
Zeyuan Hu <ferrishu3886@gmail.com>
2018-01-18 20:16:45 -0600

    Add 102

---
+++ leetcode/102-BinaryTreeLevelOrderTraversal/README.md
@@ -0,0 +1,9 @@
+## Solution explanation
+
+The key part of this solution is to utilize the length of the queue to know the number of nodes on
+each level. Utilizing the length of the queue is a very important trick in BFS. "17-Letter Combination Of A Phone Number"
+is another example.
+
+## Reference
+
+- https://leetcode.com/problems/binary-tree-level-order-traversal/discuss/33450
---
+++ leetcode/102-BinaryTreeLevelOrderTraversal/binaryTreeLevelOrderTraversal.py
@@ -0,0 +1,47 @@
+# Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+#
+# For example:
+# Given binary tree [3,9,20,null,null,15,7],
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+# return its level order traversal as:
+# [
+#   [3],
+#   [9,20],
+#   [15,7]
+# ]
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        res = []
+        if not root:
+            return res
+        else:
+            queue = []
+            queue.insert(0, root)
+            while queue:
+                path = []
+                cnt = len(queue)
+                for _ in range(cnt):
+                    node = queue.pop()
+                    path.append(node.val)
+                    if node.left:
+                        queue.insert(0, node.left)
+                    if node.right:
+                        queue.insert(0, node.right)
+                res.append(path)
+            return res
