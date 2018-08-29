1d734ec1aa9a760b04e08fe9520dc9ea90627bb6
Zeyuan Hu <ferrishu3886@gmail.com>
2018-08-28 19:28:08 -0500

    Remove redundant code

--- misc/EarliestValidTime/earliestValidTime.py
+++ misc/EarliestValidTime/earliestValidTime.py
@@ -5,21 +5,21 @@ def solution(A, B, C, D, E, F):
     # write your code in Python 3.6
     d = [A, B, C, D, E, F]
     d = sorted(d)
-    if (d[4] < 6):
+    if d[4] < 6:
         # 2nd largest digit is smaller 6, we can just fill up
-        if (10 * d[0] + d[1] < 24):
+        if 10 * d[0] + d[1] < 24:
             return f'{d[0]}{d[1]}:{d[2]}{d[3]}:{d[4]}{d[5]}'
         else:
             return "NOT POSSIBLE"
-    elif (d[3] < 6):
+    elif d[3] < 6:
         # 3rd largest digit is smaller 6, put 2nd largest in 4th position
-        if (10 * d[0] + d[1] < 24):
+        if 10 * d[0] + d[1] < 24:
             return f'{d[0]}{d[1]}:{d[2]}{d[4]}:{d[3]}{d[5]}'
         else:
             return "NOT POSSIBLE"
-    elif (d[2] < 6):
+    elif d[2] < 6:
         # 4th largest digit is smaller 6, put 3rd largest in 2nd position
-        if (10 * d[0] + d[3] < 24):
+        if 10 * d[0] + d[3] < 24:
             return f'{d[0]}{d[3]}:{d[1]}{d[4]}:{d[2]}{d[5]}'
         else:
             return "NOT POSSIBLE"
