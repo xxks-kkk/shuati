28e9b8ca0da4589c019fc76aa4699339f685f991
Zeyuan Hu <ferrishu3886@gmail.com>
2018-09-24 11:36:14 -0500

    Add 379

---
+++ leetcode/379-DesignPhoneDirectory/designPhoneDirectory.py
@@ -0,0 +1,46 @@
+class PhoneDirectory:
+
+    def __init__(self, maxNumbers):
+        """
+        Initialize your data structure here
+        @param maxNumbers - The maximum numbers that can be stored in the phone directory.
+        :type maxNumbers: int
+        """
+        self.available = set(range(maxNumbers))
+
+    def get(self):
+        """
+        Provide a number which is not assigned to anyone.
+        @return - Return an available number. Return -1 if none is available.
+        :rtype: int
+        """
+        if self.available:
+            return self.available.pop()
+        else:
+            return -1
+
+    def check(self, number):
+        """
+        Check if a number is available or not.
+        :type number: int
+        :rtype: bool
+        """
+        return number in self.available
+
+    def release(self, number):
+        """
+        Recycle or release a number.
+        :type number: int
+        :rtype: void
+        """
+        return self.available.add(number)
+
+# Your PhoneDirectory object will be instantiated and called as such:
+# obj = PhoneDirectory(maxNumbers)
+# param_1 = obj.get()
+# param_2 = obj.check(number)
+# obj.release(number)
+
+
+if __name__ == "__main__":
+    pass
