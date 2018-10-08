7c4e11e3f8d89617d92bc449c700e97f9f4ae7cf
Zeyuan Hu <ferrishu3886@gmail.com>
2018-10-07 21:41:20 -0500

    Update 285 C solution

---
+++ leetcode/cinclude/bst.c
@@ -0,0 +1,139 @@
+#include "bst.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+struct TreeNode *
+insertNode(struct TreeNode *r, int i)
+{
+    struct TreeNode *n = malloc(sizeof(struct TreeNode));
+    if (n == NULL)
+    {
+        return n;
+    }
+    n->left = n->right = n->parent = NULL;
+    n->val = i;
+
+    if (r == NULL)
+    {
+        return n;
+    }
+
+    do
+    {
+        n->parent = r;
+        if (n->val < r->val)
+        {
+            r = r->left;
+        }
+        else if (n->val > r->val)
+        {
+            r = r->right;
+        }
+        else
+        {
+            free(n);
+            return NULL;
+        }
+    }
+    while (r != NULL);
+
+    if (n->val < n->parent->val)
+    {
+        n->parent->left = n;
+    }
+    else
+    {
+        n->parent->right = n;
+    }
+
+    return n;
+}
+
+struct TreeNode *
+buildTree(int *t, int count)
+{
+    if (count <= 0)
+    {
+        return NULL;
+    }
+    struct TreeNode *root = insertNode(NULL, t[0]);
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    for (int i = 1; i < count && insertNode(root, t[i]) != NULL; ++i);
+    return root;
+}
+
+/* Example BST:
+ *
+ *       8
+ *     /   \
+ *    4     12
+ *   / \    / \
+ *  1   7 11   20
+ *
+ * struct TreeNode {
+ *    int value;
+ *    struct TreeNode *left, *right, *parent;
+ * };
+ */
+
+struct TreeNode *
+findMin(struct TreeNode *n)
+{
+    struct TreeNode *current = n;
+    if (current == NULL)
+    {
+        return current;
+    }
+    while (current->left != NULL)
+    {
+        current = current->left;
+    }
+    return current;
+}
+
+/***
+ * This function performs the same task as LC285. However, unlike 285,
+ * it only needs one parameter, a node that needs to be searched for its successor. The reason we can do so
+ * because we have access to the parent pointer.
+ * @param n a given node
+ * @return the successor of the given node
+ */
+struct TreeNode *
+findNext(struct TreeNode *n)
+{
+    if (n->right != NULL)
+    {
+        return findMin(n->right);
+    }
+    struct TreeNode *p = n->parent;
+    while (p != NULL && n == p->right)
+    {
+        n = p;
+        p = p->parent;
+    }
+    return p;
+}
+
+void
+printBst(struct TreeNode *n)
+{
+    struct TreeNode *current = findMin(n);
+    if (current != NULL)
+    {
+        printf("%d\n", current->val);
+    }
+    do
+    {
+        current = findNext(current);
+        if (current != NULL)
+        {
+            printf("%d\n", current->val);
+        }
+    }
+    while (current != NULL);
+}
+
---
+++ leetcode/cinclude/bst.h
@@ -0,0 +1,25 @@
+#ifndef BST_H
+#define BST_H
+
+struct TreeNode
+{
+    int val;
+    struct TreeNode *left, *right, *parent;
+};
+
+struct TreeNode *
+findMin(struct TreeNode *n);
+
+struct TreeNode *
+findNext(struct TreeNode *n);
+
+void
+printBst(struct TreeNode *n);
+
+struct TreeNode *
+insertNode(struct TreeNode *r, int i);
+
+struct TreeNode *
+buildTree(int *t, int count);
+
+#endif
