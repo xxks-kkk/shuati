d11e87b509e3437797a97486564b3772ffcef067
Zeyuan Hu <ferrishu3886@gmail.com>
2018-10-13 23:53:12 -0500

    Add 214

--- misc/findAncestor/findAncestor.cc
+++ misc/findAncestor/findAncestor.cc
@@ -9,7 +9,8 @@ using namespace std;
 
 // Databricks Phone Interview
 
-// Suppose we have some input data describing a graph of relationships between parents and children over multiple generations. The data is formatted as a list of (parent, child) pairs, where each individual is assigned a unique integer identifier.
+// Suppose we have some input data describing a graph of relationships between parents and children over multiple generations.
+// The data is formatted as a list of (parent, child) pairs, where each individual is assigned a unique integer identifier.
 
 // For example, in this diagram, 3 is a child of 1 and 2, and 5 is a child of 4:
 
