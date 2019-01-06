6b6676222a8b9353e14dc152faeddb88b99a1ff3
xxks-kkk <ferrishu3886@gmail.com>
2019-01-06 15:31:33 -0600

    Add rust support into project

---
+++ leetcode/1-TwoSum/TwoSum.rs
@@ -0,0 +1,26 @@
+use std::collections::HashMap;
+
+#[allow(dead_code)]
+struct Solution {}
+
+impl Solution {
+    #[allow(dead_code)]
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut dict: HashMap<i32, i32> = HashMap::new();
+        for idx in 0..nums.len() {
+            if dict.contains_key(&(target - nums[idx])) {
+                return vec![dict[&(target - nums[idx])], idx as i32];
+            } else {
+                dict.insert(nums[idx], idx as i32);
+            }
+        }
+        panic!("No solution to TwoSum");
+    }
+}
+
+#[test]
+fn test() {
+    let nums = vec![2, 7, 11, 15];
+    let target = 9;
+    assert_eq!(Solution::two_sum(nums, target), vec![0, 1]);
+}
---
+++ leetcode/src/lib.rs
@@ -0,0 +1,3 @@
+#[allow(non_snake_case)]
+#[path="../1-TwoSum/TwoSum.rs"]
+mod TwoSum;
