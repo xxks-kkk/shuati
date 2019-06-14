6646a4046553098d15366c10c504e098086996f4
xxks-kkk <ferrishu3886@gmail.com>
2019-06-14 17:13:25 -0500

    Add 832

---
+++ leetcode/832-FlippingAnImage/flippingAnImage.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+# Given a binary matrix A, we want to flip the image horizontally, then invert it, and return the resulting image.
+
+# To flip an image horizontally means that each row of the image is reversed.  For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
+
+# To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0. For example, inverting [0, 1, 1] results in [1, 0, 0].
+
+# Example 1:
+
+# Input: [[1,1,0],[1,0,1],[0,0,0]]
+# Output: [[1,0,0],[0,1,0],[1,1,1]]
+# Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+# Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+
+# Example 2:
+
+# Input: [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+# Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+# Explanation: First reverse each row: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+# Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+
+# Notes:
+
+#     1 <= A.length = A[0].length <= 20
+#     0 <= A[i][j] <= 1
+
+# NOTE: https://docs.python.org/3/library/typing.html
+from typing import List
+
+
+class Solution:
+    def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
+        sol = []
+        for row in A:
+            for i in range(len(row)):
+                if row[i] == 1:
+                    row[i] = 0
+                elif row[i] == 0:
+                    row[i] = 1
+            sol.append(list(reversed(row)))
+        return sol
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    A = [[1, 1, 0], [1, 0, 1], [0, 0, 0]]
+    assert sol.flipAndInvertImage(A) == [[1, 0, 0], [0, 1, 0], [1, 1, 1]]
+    A = [[1, 1, 0, 0], [1, 0, 0, 1], [0, 1, 1, 1], [1, 0, 1, 0]]
+    assert sol.flipAndInvertImage(A) == [
+        [1, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 1],
+        [1, 0, 1, 0],
+    ]
