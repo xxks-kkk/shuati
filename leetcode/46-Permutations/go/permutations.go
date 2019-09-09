f2ee2073e9641928e761212c0838c83da66dbe41
xxks-kkk <ferrishu3886@gmail.com>
2019-09-09 17:40:30 -0500

    Add implementation but OJ returns different result than the local test

---
+++ leetcode/46-Permutations/go/permutations.go
@@ -0,0 +1,45 @@
+package permutations
+
+// Given a collection of distinct integers, return all possible permutations.
+
+// Example:
+
+// Input: [1,2,3]
+// Output:
+// [
+//   [1,2,3],
+//   [1,3,2],
+//   [2,1,3],
+//   [2,3,1],
+//   [3,1,2],
+//   [3,2,1]
+// ]
+
+func permute(nums []int) [][]int {
+	var tmp []int
+	var res [][]int
+	invalid_pos := make([]bool, len(nums))
+	res = backtrack(nums, tmp, invalid_pos, res)
+	return res
+}
+
+// backtrack generates all the permutations of the given `nums` and put them into `res`.
+// `invalid_pos` keeps track of which number is valid to pick from `nums` to form a permutation
+// `tmp`
+func backtrack(nums []int, tmp []int, invalid_pos []bool, res [][]int) [][]int {
+	if len(tmp) == len(nums) {
+		res = append(res, tmp)
+	} else {
+		for i, num := range nums {
+			if invalid_pos[i] {
+				continue
+			}
+			invalid_pos[i] = true
+			tmp = append(tmp, num)
+			res = backtrack(nums, tmp, invalid_pos, res)
+			invalid_pos[i] = false
+			tmp = tmp[:len(tmp)-1]
+		}
+	}
+	return res
+}
---
+++ leetcode/46-Permutations/go/permutations_test.go
@@ -0,0 +1,21 @@
+package permutations
+
+import (
+	"github.com/xxks-kkk/shuati/leetcode/goinclude"
+	"testing"
+)
+
+func TestPermute(t *testing.T) {
+	var tests = []struct {
+		nums []int
+		want [][]int
+	}{
+		{[]int{1, 2, 3}, [][]int{{1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}}},
+		{[]int{5, 4, 6, 2}, [][]int{{5, 4, 6, 2}, {5, 4, 2, 6}, {5, 6, 4, 2}, {5, 6, 2, 4}, {5, 2, 4, 6}, {5, 2, 6, 4}, {4, 5, 6, 2}, {4, 5, 2, 6}, {4, 6, 5, 2}, {4, 6, 2, 5}, {4, 2, 5, 6}, {4, 2, 6, 5}, {6, 5, 4, 2}, {6, 5, 2, 4}, {6, 4, 5, 2}, {6, 4, 2, 5}, {6, 2, 5, 4}, {6, 2, 4, 5}, {2, 5, 4, 6}, {2, 5, 6, 4}, {2, 4, 5, 6}, {2, 4, 6, 5}, {2, 6, 5, 4}, {2, 6, 4, 5}}},
+	}
+	for _, test := range tests {
+		if got := permute(test.nums); !goinclude.IntNestedSliceCompare(got, test.want) {
+			t.Errorf("permute(%q) = %v", test.nums, got)
+		}
+	}
+}
