c5c757b6a4404ebbea3af93a71d9ff797218e034
xxks-kkk <ferrishu3886@gmail.com>
2019-09-10 03:40:36 +0000

    Update comment

--- leetcode/46-Permutations/go/permutations.go
+++ leetcode/46-Permutations/go/permutations.go
@@ -28,6 +28,12 @@ func permute(nums []int) [][]int {
 // `tmp`
 func backtrack(nums []int, tmp []int, invalid_pos []bool, res [][]int) [][]int {
 	if len(tmp) == len(nums) {
+		// NOTE: make a copy to make leetcode OJ pass; not sure why I need to make a copy
+		// here. From Leetcode result, I'm suspecting a "Capturing Iteration Variable"
+		// (https://stackoverflow.com/questions/52980172/cannot-understand-5-6-1-caveat-capturing-iteration-variables)
+		// situation can happen. However, I cannot test it out given my current test implementation.
+		// Same implementation strategy (e.g., append(res, tmp)) is used in LC-401. However, OJ pass in that problem.
+		// I think it might be an error for OJ?
 		tmp2 := make([]int, len(tmp))
 		copy(tmp2, tmp)
 		res = append(res, tmp2)
