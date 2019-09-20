4a63152c5104ac6fa1c18aa2afc0835ea0f14307
Zeyuan Hu <ferrishu3886@gmail.com>
2019-09-19 20:44:57 -0500

    [LC977][C++][Two Pointers][v1] (#133)
    * Add solution using sort
    
    * Add two pointers solution

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,8 @@
-((nil . ((projectile-project-root . "/Users/zyh/Documents/projects/shuati"))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (projectile-project-root . "/Users/zyh/Documents/projects/shuati"))
+ (c++-mode
+  (flycheck-clang-language-standard . "c++11")))
+
