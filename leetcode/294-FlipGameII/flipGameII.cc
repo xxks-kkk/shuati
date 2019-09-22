e6d4ab8a3dba9d4ad800f04dc55ec0eb1d3b193b
xxks-kkk <ferrishu3886@gmail.com>
2019-09-22 00:10:40 -0500

    Add recursion solution

--- leetcode/294-FlipGameII/flipGameII.cc
+++ leetcode/294-FlipGameII/flipGameII.cc
@@ -8,81 +8,19 @@ using namespace std;
 class Solution {
 public:
     bool canWin(string s) {
-        int depth = 0;
-        return backtrack(depth, s);
-    }
-private:
-  bool backtrack(int depth, string s) {
-    int s_size = s.length();
-    if (s_size == 0 || s_size == 1) {
+      for (int i = 1; i < s.size(); ++i) {
+        if (s[i] == '+' && s[i - 1] == '+' && !canWin(s.substr(0, i - 1) + "--" + s.substr(i + 1))) {
+          return true;
+        }
+      }
       return false;
     }
-    vector<string> states = generateStates(s);
-    if (depth % 2 == 1 && states.empty()) {
-      return true;
-    } else if (depth % 2 == 0 && states.empty()) {
-      return false;
-    }
-    for (auto&& state: states) {
-      auto res = backtrack(depth+1, state);
-      if (res) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  // This generate all possible states by replacing "++" into "--"
-  // if possible
-  vector<string> generateStates(string s) {
-    vector<string> res;
-    if (s.length() == 0) {
-      return res;
-    }
-    for(int i = 0; i < (signed) s.length()-1; ++i) {
-      if (s[i] == '+' && s[i+1] == '+') {
-        string tmp = s;
-        tmp.replace(i, 2, "--");
-        res.push_back(tmp);
-      }
-    }
-    return res;
-  }
-
-  friend class Test;
 };
 
 using ptr2canWin = bool (Solution::*)(string);
 
 class Test {
 public:
-  void test_generateStates() {
-    Solution sol;
-    struct testCase {
-      string s;
-      vector<string> expected;
-    };
-    vector<testCase> test_cases = {
-      { "", {}},
-      {"++++", {"--++", "++--", "+--+"}},
-    };
-    for(auto && test_case: test_cases) {
-      vector<string> got = (sol.generateStates)(test_case.s);
-      for(auto&& item: got) {
-        if (find(test_case.expected.begin(), test_case.expected.end(), item) == test_case.expected.end())
-        {
-          string got_str = "[";
-          for(auto && item: got) {
-            got_str += item;
-            got_str += ", ";
-          }
-          got_str += "]";          
-          printf("generateStates(%s) = %s", test_case.s.c_str(), got_str.c_str());
-        }
-      }
-    }
-  }
-
   void test_pfcn(ptr2canWin pfcn) {
     Solution sol;
     struct testCase {
@@ -109,7 +47,6 @@ public:
 
 int main() {
   Test test;
-  test.test_generateStates();
   ptr2canWin pfcn = &Solution::canWin;
   test.test_pfcn(pfcn);
 }
