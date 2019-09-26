495db2005cd6b85ca1675f5dd8d8ff1f58477faf
xxks-kkk <ferrishu3886@gmail.com>
2019-09-26 16:44:44 -0500

    Add dfs (with modifying board) version; much cleaner than dfs (wihtout modifying board)

--- leetcode/200-NumberOfIslands/numberOfIslands.cc
+++ leetcode/200-NumberOfIslands/numberOfIslands.cc
@@ -151,30 +151,44 @@ public:
     }
     return uf.getCount();
   }
-private:
-    void
-    dfs(vector<vector<char>> &grid, int i, int j)
-    {
-        grid[i][j] = 'b';
-        int m = grid.size();
-        int n = grid[0].size();
-        if (i > 0 && grid[i-1][j] == '1')
-        {
-            dfs(grid, i-1, j);
-        }
-        if (i < m-1 && grid[i+1][j] == '1')
-        {
-            dfs(grid, i+1, j);
-        }
-        if (j > 0 && grid[i][j-1] == '1')
-        {
-            dfs(grid, i, j-1);
-        }
-        if (j < n-1 && grid[i][j+1] == '1')
-        {
-            dfs(grid, i, j+1);
-        }
+
+  // dfs (with modifying board)
+  // note: this version is much cleaner than dfs (wihtout modifying board) version
+  //       If interviewer not noted, use this version
+  int numIslands3(vector<vector<char>>& grid) {
+    if (grid.empty()) return 0;
+    int m = grid.size();
+    int n = grid[0].size();
+    int count = 0;
+    for(int i = 0; i < m; i++) {
+      for(int j = 0; j < n; j++) {
+        count += grid[i][j] - '0';
+        dfs3(grid, i, j, m, n);
+      }
     }
+    return count;
+  }
+private:
+  void dfs(vector<vector<char>> &grid, int i, int j)
+  {
+    grid[i][j] = 'b';
+    int m = grid.size();
+    int n = grid[0].size();
+    if (i > 0 && grid[i-1][j] == '1')   dfs(grid, i-1, j);
+    if (i < m-1 && grid[i+1][j] == '1') dfs(grid, i+1, j);
+    if (j > 0 && grid[i][j-1] == '1')   dfs(grid, i, j-1);
+    if (j < n-1 && grid[i][j+1] == '1') dfs(grid, i, j+1);
+  }
+
+  void dfs3(vector<vector<char>> &grid, int i, int j, int m, int n)
+  {
+    if (grid[i][j] == '0') return;
+    grid[i][j] = '0';
+    if (i > 0 && grid[i-1][j] == '1')   dfs3(grid, i-1, j, m, n);
+    if (i < m-1 && grid[i+1][j] == '1') dfs3(grid, i+1, j, m, n);
+    if (j > 0 && grid[i][j-1] == '1')   dfs3(grid, i, j-1, m, n);
+    if (j < n-1 && grid[i][j+1] == '1') dfs3(grid, i, j+1, m, n);    
+  }
 };
 
 using ptr2numIslands = int (Solution::*)(vector<vector<char>> &);
@@ -186,7 +200,7 @@ struct testFuncsInfo{
 };
 
 void
-test(vector<testFuncsInfo>& func_arrays)
+test(ptr2numIslands pfcn, const char* func_name, bool check_grid)
 {
     Solution sol;
     struct testCase {
@@ -207,26 +221,24 @@ test(vector<testFuncsInfo>& func_arrays)
         {'0', '0', '0', '1', '1'}
         }, 3},
     };
-    for(auto&& func: func_arrays) {
-      for(auto&& test_case: test_cases) {
-        auto grid2(test_case.grid);
-        int got = (sol.*(func.pfcn))(test_case.grid);
-        if (got != test_case.expected) {
-          // check if the function result is correct
-          printf("%s(%s) = %s\n",
-                 func.func_name,
-                 CPPUtility::matrixStr(test_case.grid).c_str(),
-                 to_string(got).c_str());
-        }
-        if (func.check_grid && grid2 != test_case.grid) {
-          // check if board is modified after invoking function
-          printf("%s(%s) modified grid: %s\n",
-                 func.func_name,
-                 CPPUtility::matrixStr(test_case.grid).c_str(),
-                 CPPUtility::matrixStr(grid2).c_str()
-            );
-        }
-      }      
+    for(auto&& test_case: test_cases) {
+      auto grid2(test_case.grid);
+      int got = (sol.*pfcn)(test_case.grid);
+      if (got != test_case.expected) {
+        // check if the function result is correct
+        printf("%s(%s) = %s\n",
+               func_name,
+               CPPUtility::matrixStr(grid2).c_str(),
+               to_string(got).c_str());
+      }
+      if (check_grid && grid2 != test_case.grid) {
+        // check if board is modified after invoking function
+        printf("%s(%s) modified grid: %s\n",
+               func_name,
+               CPPUtility::matrixStr(grid2).c_str(),
+               CPPUtility::matrixStr(test_case.grid).c_str()
+          );
+      }
     }
 };
 
@@ -236,6 +248,9 @@ main()
   vector<testFuncsInfo> func_array = {
     FUNC_DEF(&Solution::numIslands, true)
     FUNC_DEF(&Solution::numIslands2, false)
+    FUNC_DEF(&Solution::numIslands3, false)
   };
-  test(func_array);
+  for(auto&& func : func_array) {
+    test(func.pfcn, func.func_name, func.check_grid);
+  }
 }
