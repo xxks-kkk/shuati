aaa2915ec5509cc4d4a3c2f573d1f46e22047182
xxks-kkk <ferrishu3886@gmail.com>
2019-10-09 21:25:51 -0500

    Pass existing test case found online for buyFruits.cc

--- misc/Amazon/buyFruits.cc
+++ misc/Amazon/buyFruits.cc
@@ -1,31 +1,89 @@
 #include "cpputility.h"
+#include <unordered_map>
 
 using namespace std;
 
+void dfs(vector<string>& tmp,
+         vector<int>& visited,
+         vector<vector<string>>& res,
+         const vector<string>& fruits,
+         int anything_pos_size) {
+  if (tmp.size() == anything_pos_size) {
+    res.push_back(tmp);
+    return;
+  }
+  for(int i = 0; i < fruits.size(); ++i) {
+    if (visited[i] == 0) {
+      tmp.push_back(fruits[i]);
+      visited[i] = 1;
+      dfs(tmp, visited, res, fruits, anything_pos_size);
+      visited[i] = 0;
+      tmp.pop_back();
+    }
+  }
+}
+
+vector<vector<string>> generate_cands(vector<string> code, const unordered_map<string,int>& m) {
+  vector<int> anything_pos;
+  vector<vector<string>> res;
+  for(int i = 0; i < code.size(); ++i) {
+    if (code[i] == "anything") {
+      anything_pos.push_back(i);
+    }
+  }
+  if (anything_pos.size() == 0) {
+    res.push_back(code);
+    return res;
+  }
+  vector<string> fruits;
+  for(auto&& item: m) {
+    fruits.push_back(item.first);
+  }
+  vector<int> visited(fruits.size(), 0);
+  vector<string> tmp;
+  vector<vector<string>> cands;
+  dfs(tmp, visited, cands, fruits, anything_pos.size());
+  for(auto&& v : cands) {
+    for(int i = 0; i < anything_pos.size(); ++i) {
+      code[anything_pos[i]] = v[i];
+    }
+    res.push_back(code);
+  }
+  return res;
+}
+
 // NOTE: the actual problem may be based on the Linked List
 int checkWinner (vector<vector<string>> codeList, vector<string> shoppingCart) {
+  unordered_map<string, int> m;
+  for(auto&& fruit: shoppingCart) {
+    m[fruit]++;
+  }
+  int num_kind_fruit = m.size();
   int matched_index = 0;
   for(auto&& code: codeList) {
     int code_size = code.size();
     int i;
     for(i = 0; i <= shoppingCart.size() - code_size; ++i) {
+      auto code_set = generate_cands(code, m);
       vector<string> v(shoppingCart.begin() + i, shoppingCart.begin() + i + code_size);
-      if (v == code) {
-        if (i >= matched_index) {
-          matched_index = i;
-          break;
-        } else {
-          return 0;
-        }
+      for(auto&& cand: code_set) {
+        if (v == cand) {
+          if (i >= matched_index) {
+            matched_index = i;
+            goto endloop;
+          } else {
+            return 0;
+          }
+        }        
       }
     }
+  endloop:    
     if (i == shoppingCart.size() - code_size + 1) {
       return 0;
     }
   }
   return 1;
 }
-
 
 using ptr2checkWinner = int (*) (vector<vector<string>>, vector<string>);
 
@@ -47,6 +105,9 @@ void test(ptr2checkWinner pfcn, const char* pfcn_name) {
      0},
     {{{"apple", "apple"}, {"orange", "anything", "orange"}},
      {"orange", "apple", "apple", "orange", "mango", "orange"},
+     1},
+    {{{"apple", "apple"}, {"orange", "banana", "orange"}},
+     {"orange", "apple", "apple", "orange", "apple", "orange", "banana", "orange"},
      1},
   };
   for(auto&& test_case: test_cases) {
