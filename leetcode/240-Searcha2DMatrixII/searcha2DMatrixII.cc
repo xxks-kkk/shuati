e1854631101f69a5ebf99708efc554f5ca69bd89
Zeyuan Hu <ferrishu3886@gmail.com>
2019-10-22 23:13:08 -0500

    [LC240][C++][Pruning based on Observation][v1] (#159)
    * [LC240][C++][Observation][v1]
    
    * Optimize implementation

---
+++ leetcode/240-Searcha2DMatrixII/searcha2DMatrixII.cc
@@ -0,0 +1,68 @@
+#include<vector>
+#include "cpputility.h"
+
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+      if (matrix.empty() || matrix[0].empty()) return false;
+      int num_rows = matrix.size();
+      int num_cols = matrix[0].size();
+      int i = num_rows - 1;
+      int j = 0;
+      while(i >= 0 && j < num_cols) {
+          int element = matrix[i][j];
+          if (element == target) {
+            return true;
+          } else if (element > target) {
+            i--;
+          } else if (element < target) {
+            j++;
+          }        
+      }
+      return false;
+    }
+};
+
+using ptr2searchMatrix = bool (Solution::*)(vector<vector<int>>&, int);
+
+struct testFuncsInfo {
+  ptr2searchMatrix pfcn;
+  const char* pfcn_name;
+};
+
+void test(ptr2searchMatrix pfcn, const char* pfcn_name) {
+  Solution sol;
+  struct testCase {
+    vector<vector<int>> matrix;
+    int target;
+    bool expected;
+  };
+  vector<testCase> test_cases = {
+    {{{1,4,7,11,15}, {2,5,8,12,19}, {3,6,9,16,22}, {10,13,14,17,24}, {18,21,23,26,30}}, 5, true},
+    {{{1,4,7,11,15}, {2,5,8,12,19}, {3,6,9,16,22}, {10,13,14,17,24}, {18,21,23,26,30}}, 20, false},
+    {{{}}, 10, false},
+  };
+  for(auto&& test_case: test_cases) {
+    bool got = (sol.*pfcn)(test_case.matrix, test_case.target);
+    if (got != test_case.expected) {
+      printf("%s(%s,%d) = %s\n",
+             pfcn_name,
+             CPPUtility::twoDVectorStr<int>(test_case.matrix).c_str(),
+             test_case.target,
+             got == true ? "true": "false");
+      assert(false);
+    }
+  }
+}
+
+int main() {
+  vector<testFuncsInfo> func_array = {
+    FUNC_DEF(&Solution::searchMatrix)
+  };
+  for(auto&& func : func_array) {
+    test(func.pfcn, func.pfcn_name);
+  }  
+}
+  
