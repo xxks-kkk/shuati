e924e5b7a2ef0f1c06482abf6f39c9ddb9f99f4b
xxks-kkk <ferrishu3886@gmail.com>
2019-10-29 14:18:27 -0500

    Use member initializer list constructor format

--- include/cppinclude/linkedList_rand.h
+++ include/cppinclude/linkedList_rand.h
@@ -4,31 +4,28 @@
 #include "shared_headers.h"
 
 // Definition for a Node in linked list with random pointer
-class Node {
+class Node
+{
 public:
-    int val;
-    Node* next;
-    Node* random;
+  int val;
+  Node *next;
+  Node *random;
 
-    Node() {}
-
-    Node(int _val, Node* _next, Node* _random) {
-        val = _val;
-        next = _next;
-        random = _random;
-    }
+  Node() {}
+  Node(int _val, Node *_next, Node *_random) : val(_val), next(_next), random(_random) {}
+// private:
+//   std::unique_ptr<Node> node;
 };
 
 class LinkedListRandom
 {
 public:
   // transform the json representation of linked list into the linked list
-  static Node* list2list(std::string  rawJson);
+  static Node *list2list(std::string rawJson);
   // transform a linked list to the json representation of linked list
-  static std::string printList(Node* head);
+  static std::string printList(Node *head);
   // delete the linked list
-  static void freeList(Node* &head);
+  static void freeList(Node *&head);
 };
-
 
 #endif
