492d153fdd57740cb6d9e29b89167dc6f42a305b
xxks-kkk <ferrishu3886@gmail.com>
2019-11-03 11:37:36 -0800

    Further reduce TLE solution from 240ms in worst case test case to 8ms with the same test case; now TLE solution is accepted by OJ

--- leetcode/5-LongestPalindromicSubstring/longestPalindromicSubstring.cc
+++ leetcode/5-LongestPalindromicSubstring/longestPalindromicSubstring.cc
@@ -6,31 +6,26 @@ using namespace std;
 class Solution {
 public:
   string longestPalindrome(string s) {
-    string res = "";
-    for(int i = 0; i < s.length(); ++i) {
-      auto cand1 = longestPalindromeHelper(s, i, i);
-      auto cand2 = longestPalindromeHelper(s, i, i+1);
-      if (cand1.length() > cand2.length()) {
-        if (res.length() < cand1.length()) res = cand1;
-      } else {
-        if (res.length() < cand2.length()) res = cand2;
+    // invariant: s[start, end] (inclusive) is the longest palindrome    
+    int start = 0, end = 0; 
+    for(int i = 0; i < s.length(); ++i) {      
+      auto len1 = longestPalindromeHelper(s, i, i);
+      auto len2 = longestPalindromeHelper(s, i, i+1);
+      auto len = max(len1, len2);
+      if (len > end - start + 1) {
+        start = i - (len-1) / 2;
+        end = i + len / 2;
       }
     }
-    return res;
+    return s.substr(start, end-start+1);
   }
 private:
-  string longestPalindromeHelper(const string& s, int left, int right) {
-    string res = "";
+  int longestPalindromeHelper(const string& s, int left, int right) {
     while(left >= 0 && right < s.length() && s[left] == s[right]) {
-      if (left == right) res += s[right];
-      else {
-        res = s[left] + res;
-        res += s[right];
-      }
       left--;
       right++;
     }
-    return res;
+    return right-left-1;
   }
 };
 
