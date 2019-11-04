6ad5f156f30f3ae6038a8d17628df035c73e18c4
xxks-kkk <ferrishu3886@gmail.com>
2019-11-03 17:47:53 -0800

    Add DP solution

--- leetcode/5-LongestPalindromicSubstring/longestPalindromicSubstring.cc
+++ leetcode/5-LongestPalindromicSubstring/longestPalindromicSubstring.cc
@@ -5,6 +5,7 @@ using namespace std;
 
 class Solution {
 public:
+  // Recursion
   string longestPalindrome(string s) {
     // invariant: s[start, end] (inclusive) is the longest palindrome    
     int start = 0, end = 0; 
@@ -19,6 +20,26 @@ public:
     }
     return s.substr(start, end-start+1);
   }
+
+  // DP
+  string longestPalindrome2(string s) {
+    int palindromeBeginsAt = 0;
+    int maxLength = 0;
+    int n = static_cast<int>(s.length());
+    // DP[i][j] = 1 means a palindrome starts at i and ends at j (inclusive)
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+    for(int i = n-1; i >= 0; --i) {
+      for(int j = i; j < n; ++j) {
+        dp[i][j] = (s[i] == s[j]) && (j-i <= 2 || dp[i+1][j-1]);
+        if (dp[i][j] && j-i+1 > maxLength) {
+          palindromeBeginsAt = i;
+          maxLength = j-i+1;
+        }
+      }
+    }
+    return s.substr(palindromeBeginsAt, maxLength);
+  }
+  
 private:
   int longestPalindromeHelper(const string& s, int left, int right) {
     while(left >= 0 && right < s.length() && s[left] == s[right]) {
@@ -40,15 +61,15 @@ void test(ptr2longestPalindrome pfcn, const char* pfcn_name) {
   Solution sol;
   struct testCase {
     string s;
-    string expected;
+    vector<string> expected;
   };
   vector<testCase> test_cases = {
-    {"babad", "bab"},
-    {"cbbd", "bb"},
+    {"babad", {"aba","bab"}},
+    {"cbbd", {"bb"}},
   };
   for(auto&& test_case: test_cases) {
     auto got = (sol.*pfcn)(test_case.s);
-    if (got != test_case.expected) {
+    if (find(test_case.expected.begin(), test_case.expected.end(), got) == test_case.expected.end()) {
       printf("%s(%s) = %s\n",
              pfcn_name,
              test_case.s.c_str(),
@@ -61,6 +82,7 @@ void test(ptr2longestPalindrome pfcn, const char* pfcn_name) {
 int main() {
   vector<testFuncsInfo> func_array = {
     FUNC_DEF(&Solution::longestPalindrome)
+    FUNC_DEF(&Solution::longestPalindrome2)
   };
   for(auto&& func: func_array) {
     test(func.pfcn, func.pfcn_name);
