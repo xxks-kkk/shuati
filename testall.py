12e85c4caf434c39d7f4ba2739bd3041e35897d1
xxks-kkk <ferrishu3886@gmail.com>
2019-11-20 21:35:12 -0600

    Start to use testall.py instead of testall.sh

---
+++ testall.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+
+"""
+This script ochestrate all the source file test for C++/C
+"""
+
+
+def testall():
+    """
+    Test all the generated binary of C++/C
+    """
+    pass
+
+
+if __name__ == "__main__":
+    testall()
