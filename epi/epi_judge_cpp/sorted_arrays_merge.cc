06e835e94b75e91db7c4f1cb9d043840ec92ffca
xxks-kkk <ferrishu3886@gmail.com>
2020-01-23 14:23:30 -0800

    Add EPI-10.1

--- epi/epi_judge_cpp/sorted_arrays_merge.cc
+++ epi/epi_judge_cpp/sorted_arrays_merge.cc
@@ -1,10 +1,37 @@
 #include <vector>
+#include <queue>
 
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::priority_queue;
+using std::greater;
+
+struct IteratorCurrentAndEnd {
+  bool operator>(const IteratorCurrentAndEnd& that) const {
+    return *current > *that.current;
+  }
+
+  vector<int>::const_iterator current;
+  vector<int>::const_iterator end;
+};
+
 vector<int> MergeSortedArrays(const vector<vector<int>>& sorted_arrays) {
-  // TODO - you fill in here.
-  return {};
+  priority_queue<IteratorCurrentAndEnd, vector<IteratorCurrentAndEnd>, greater<>> min_heap;
+  for(const vector<int>& sorted_array: sorted_arrays) {
+    if (!sorted_array.empty()) {
+      min_heap.emplace(IteratorCurrentAndEnd{cbegin(sorted_array), cend(sorted_array)});
+    }
+  }
+  vector<int> result;
+  while(!min_heap.empty()) {
+    IteratorCurrentAndEnd smallest_array = min_heap.top();
+    min_heap.pop();
+    result.emplace_back(*smallest_array.current);
+    if (next(smallest_array.current) != smallest_array.end) {
+      min_heap.emplace(IteratorCurrentAndEnd{next(smallest_array.current), smallest_array.end});
+    }
+  }
+  return result;
 }
 
 int main(int argc, char* argv[]) {
--- leetcode/23-MergekSortedLists/mergekSortedLists.cc
+++ leetcode/23-MergekSortedLists/mergekSortedLists.cc
@@ -11,9 +11,9 @@
 //    Output: 1->1->2->3->4->4->5->6
 
 #include "linkedList.h"
-#include <vector>
-#include <queue>
 #include <cassert>
+#include <queue>
+#include <vector>
 
 using namespace std;
 
@@ -26,151 +26,148 @@ using namespace std;
  * };
  */
 
-// function object (p.571)
-struct comp
+bool heapCmp(ListNode *a, ListNode *b)
 {
-    // primer p.571 function-call operator
-    bool operator()(ListNode* a, ListNode* b)
-    {
-        return a->val > b->val;
-    }
-};
-
-bool heapCmp(ListNode* a, ListNode* b)
-{
-    return a->val > b->val;
+  return a->val > b->val;
 }
 
-class Solution {
+class Solution
+{
 public:
-    // Approach 1: priority_queue
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        // min-heap in C++: https://stackoverflow.com/questions/2786398/is-there-an-easy-way-to-make-a-min-heap-in-c
-        priority_queue<ListNode *, vector<ListNode*>, comp> q;
-        for(auto & l: lists)
-        {
-            if(l)
-            {
-                q.push(l);
-            }
-        }
-        if(q.empty())
-        {
-            return nullptr;
-        }
-        auto head = q.top();
-        q.pop();
-        if (head->next)
-        {
-            q.push(head->next);
-        }
-        auto current = head;
-        while(!q.empty())
-        {
-            auto element = q.top();
-            q.pop();
-            current->next = element;
-            if (element->next)
-            {
-                q.push(element->next);
-            }
-            current = current->next;
-        }
-        return head;
+  // Approach 1: priority_queue
+  ListNode *mergeKLists(vector<ListNode *> &lists)
+  {
+    // EPI p.167
+    priority_queue<ListNode *, vector<ListNode *>, function<bool(ListNode *, ListNode *)>>
+        min_heap([](ListNode *a, ListNode *b) { return a->val > b->val; });
+    for (auto &l : lists)
+    {
+      if (l)
+      {
+        min_heap.push(l);
+      }
     }
+    if (min_heap.empty())
+    {
+      return nullptr;
+    }
+    auto head = min_heap.top();
+    min_heap.pop();
+    if (head->next)
+    {
+      min_heap.push(head->next);
+    }
+    auto current = head;
+    while (!min_heap.empty())
+    {
+      auto element = min_heap.top();
+      min_heap.pop();
+      current->next = element;
+      if (element->next)
+      {
+        min_heap.push(element->next);
+      }
+      current = current->next;
+    }
+    return head;
+  }
 
-    // Approach 2: heap
-    ListNode* mergeKLists2(vector<ListNode*>& lists) {
-        auto head = new ListNode(0);
-        auto current = head;
-        vector<ListNode *> tmp;
-        for(auto & l: lists)
-        {
-            if(l)
-            {
-                tmp.push_back(l);
-            }
-        }
-        make_heap(tmp.begin(), tmp.end(), heapCmp);
-        while(!tmp.empty())
-        {
-            current->next = tmp.front();
-            // pop_heap doesn't remove element: it simply move the top element of the heap to the
-            // end of vector, which we need to call pop_back
-            pop_heap(tmp.begin(), tmp.end(), heapCmp);
-            tmp.pop_back();
-            current = current->next;
-            if (current->next)
-            {
-                tmp.push_back(current->next);
-                push_heap(tmp.begin(), tmp.end(), heapCmp);
-            }
-        }
-        return head->next;
+  // Approach 2: heap
+  ListNode *mergeKLists2(vector<ListNode *> &lists)
+  {
+    auto head = new ListNode(0);
+    auto current = head;
+    vector<ListNode *> tmp;
+    for (auto &l : lists)
+    {
+      if (l)
+      {
+        tmp.push_back(l);
+      }
     }
+    make_heap(tmp.begin(), tmp.end(), heapCmp);
+    while (!tmp.empty())
+    {
+      current->next = tmp.front();
+      // pop_heap doesn't remove element: it simply move the top element of the heap to the
+      // end of vector, which we need to call pop_back
+      pop_heap(tmp.begin(), tmp.end(), heapCmp);
+      tmp.pop_back();
+      current = current->next;
+      if (current->next)
+      {
+        tmp.push_back(current->next);
+        push_heap(tmp.begin(), tmp.end(), heapCmp);
+      }
+    }
+    return head->next;
+  }
 
-    // Approach 3: Divide-and-conquer
-    ListNode *mergeKLists3(vector<ListNode *> &lists) {
-        if(lists.empty()){
-            return nullptr;
-        }
-        while(lists.size() > 1){
-            lists.push_back(mergeTwoLists(lists[0], lists[1]));
-            lists.erase(lists.begin());
-            lists.erase(lists.begin());
-        }
-        return lists.front();
+  // Approach 3: Divide-and-conquer
+  ListNode *mergeKLists3(vector<ListNode *> &lists)
+  {
+    if (lists.empty())
+    {
+      return nullptr;
     }
+    while (lists.size() > 1)
+    {
+      lists.push_back(mergeTwoLists(lists[0], lists[1]));
+      lists.erase(lists.begin());
+      lists.erase(lists.begin());
+    }
+    return lists.front();
+  }
 
 private:
-    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
+  ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
+  {
+    if (l1 == nullptr)
     {
-        if (l1 == nullptr)
-        {
-            return l2;
-        }
-        if (l2 == nullptr)
-        {
-            return l1;
-        }
-        if (l1->val <= l2->val)
-        {
-            l1->next = mergeTwoLists(l1->next, l2);
-            return l1;
-        }
-        else
-        {
-            l2->next = mergeTwoLists(l1, l2->next);
-            return l2;
-        }
+      return l2;
     }
+    if (l2 == nullptr)
+    {
+      return l1;
+    }
+    if (l1->val <= l2->val)
+    {
+      l1->next = mergeTwoLists(l1->next, l2);
+      return l1;
+    }
+    else
+    {
+      l2->next = mergeTwoLists(l1, l2->next);
+      return l2;
+    }
+  }
 };
 
-using ptr2mergeKLists = ListNode* (Solution::*)(vector<ListNode*>&);
+using ptr2mergeKLists = ListNode *(Solution::*)(vector<ListNode *> &);
 
 void test(ptr2mergeKLists pfcn)
 {
-    Solution sol;
-    LinkedList l;
-    vector<ListNode*> example;
-    vector<int> nums = {1,4,5};
-    example.push_back(l.list2list(nums));
-    nums = {1,3,4};
-    example.push_back(l.list2list(nums));
-    nums = {2,6};
-    example.push_back(l.list2list(nums));
-    vector<int> ans = {1,1,2,3,4,4,5,6};
-    auto head = (sol.*pfcn)(example);
-    assert(l.printList(head) == ans);
-    l.freeList(head);
+  Solution sol;
+  LinkedList l;
+  vector<ListNode *> example;
+  vector<int> nums = {1, 4, 5};
+  example.push_back(l.list2list(nums));
+  nums = {1, 3, 4};
+  example.push_back(l.list2list(nums));
+  nums = {2, 6};
+  example.push_back(l.list2list(nums));
+  vector<int> ans = {1, 1, 2, 3, 4, 4, 5, 6};
+  auto head = (sol.*pfcn)(example);
+  assert(l.printList(head) == ans);
+  l.freeList(head);
 }
 
 int main()
 {
-    ptr2mergeKLists pfcn = &Solution::mergeKLists;
-    test(pfcn);
-    pfcn = &Solution::mergeKLists2;
-    test(pfcn);
-    pfcn = &Solution::mergeKLists3;
-    test(pfcn);
+  ptr2mergeKLists pfcn = &Solution::mergeKLists;
+  test(pfcn);
+  pfcn = &Solution::mergeKLists2;
+  test(pfcn);
+  pfcn = &Solution::mergeKLists3;
+  test(pfcn);
 }
