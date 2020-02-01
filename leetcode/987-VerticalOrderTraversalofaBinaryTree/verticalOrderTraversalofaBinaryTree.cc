d2b2aef3ba6b9961d3b25446c80e7eac2fe9e874
zeyuanhu <zeyuanhu@amazon.com>
2020-01-31 18:59:29 -0800

    basic setup

---
+++ leetcode/987-VerticalOrderTraversalofaBinaryTree/verticalOrderTraversalofaBinaryTree.cc
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+#include "bt.h"
+#include <vector>
+
+using std::vector;
+
+// Amazon Timetream team (01/30/2020)
+class Solution
+{
+public:
+  vector<vector<int>> verticalTraversal(TreeNode *root)
+  {
+  }
+};
+
+using ptr2verticalTraversal = vector<vector<int>> (Solution::*)(TreeNode*);
+
+void test(ptr2verticalTraversal pfcn) {
+  
+}
+
+int main()
+{
+  
+}
