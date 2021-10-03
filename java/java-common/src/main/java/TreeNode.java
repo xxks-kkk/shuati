40cb26146a6139aee7f86e451803327572eab2c4
Zeyuan Hu <ferrishu3886@gmail.com>
2021-10-02 23:19:30 -0500

    [LC1120][Java][Binary Tree] (#201)
    LC1120
    
    ** Why is this change required? **
    
    Takeaway:
    
    - We can use double[] instead of Pair for quick implementation
    - For binary tree, we can just check node == null as the only one base
      case.
    
    ** How does this change solve the issue? **
    
    ** Does this commit cause any expected behavior to change?  If so, what? **
    
    ** Issue: **
    
    ** Design Docs: **

---
+++ java/java-common/src/main/java/TreeNode.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class TreeNode
+{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    /**
+     * Convert given list of numbers into a binary tree. The numbers
+     * in the list are assumed to be ordered by level-order traversal.
+     * @return the root of the binary tree
+     */
+    public static TreeNode list2Tree(List<Integer> nums) {
+        if (!nums.isEmpty()) {
+            Queue<TreeNode> queue = new LinkedList<>();
+            TreeNode root = new TreeNode(nums.get(0));
+            queue.add(root);
+            int i = 1;
+            while (!queue.isEmpty() && i < nums.size()) {
+                TreeNode node = queue.poll();
+                TreeNode left = new TreeNode(nums.get(i));
+                queue.add(left);
+                node.left = left;
+                i++;
+                if (i < nums.size()) {
+                    TreeNode right = new TreeNode(nums.get(i));
+                    queue.add(right);
+                    node.right = right;
+                }
+                i++;
+            }
+            return root;
+        }
+        return null;
+    }
+}
---
+++ java/java-leetcode/src/main/java/MaximumAverageSubtree.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// LC-1120: https://leetcode.ca/all/1120.html
+public class MaximumAverageSubtree
+{
+    private double maximum = 0.0;
+
+    public double maximumAverageSubtree(TreeNode root) {
+        maximumAverageSubtreeHelper(root);
+        return maximum;
+    }
+
+    /**
+     * @return double[]: 1st element is number of nodes; 2nd element is subtree sum
+     */
+    private double[] maximumAverageSubtreeHelper(TreeNode root) {
+        if (root == null) {
+            return new double[]{0, 0};
+        }
+        double[] left = maximumAverageSubtreeHelper(root.left);
+        double[] right = maximumAverageSubtreeHelper(root.right);
+        double sum = root.val + left[1] + right[1];
+        double numNodes = 1 + left[0] + right[0];
+        double average = sum / numNodes;
+        if (average > maximum) {
+            maximum = average;
+        }
+        return new double[]{numNodes, sum};
+    }
+}
