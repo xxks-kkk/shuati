48dc1baedc662554517230a7e79c255c5348824e
xxks-kkk <ferrishu3886@gmail.com>
2021-10-05 01:06:08 -0500

    Add 1041

---
+++ java/java-leetcode/src/test/java/TestRobotBoundedInCircle.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TestRobotBoundedInCircle
+{
+    private RobotBoundedInCircle robotBoundedInCircle;
+
+    @BeforeAll
+    public void setUp()
+    {
+        robotBoundedInCircle = new RobotBoundedInCircle();
+    }
+
+    private static Object[][] testMaximumAverageSubtreeDataProvider() {
+        return new Object[][] {
+                {"RRGRRGLLLRLGGLGLLGRLRLGLRLRRGLGGLLRRRLRLRLLGRGLGRRRGRLG", false},
+                {"GGR", true}
+        };
+    }
+
+    @ParameterizedTest
+    @MethodSource("testMaximumAverageSubtreeDataProvider")
+    public void testMaximumAverageSubtree(String instructions, boolean expectation) {
+        assertEquals(expectation, robotBoundedInCircle.isRobotBounded(instructions));
+    }
+}
